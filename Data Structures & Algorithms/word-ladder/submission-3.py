c7c6fb69a60d08b32a839b2af09ddc90a015e4c8
mrkr188 <11003007+mrkr188@users.noreply.github.com>
2026-08-23 22:03:32 -0700

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList:
+            return 0
+
+        adj = defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + '*' + word[i+1:]
+                adj[pattern].append(word)
+
+        queue = deque([beginWord])
+        res = 1
+        while queue:
+            for i in range(len(queue)):
+                word = queue.popleft()
+                if word == endWord:
+                    return res
+                for i in range(len(word)):
+                    pattern = word[:i] + '*' + word[i+1:]
+                    
+                    for nei in adj[pattern]:
+                        # word will be present in the pattern
+                        # we dont need to add it again to queue to bfs from there
+                        if nei != word:
+                            queue.append(nei)
+                    # make pattern empty, no reason to search this path again
+                    adj[pattern] = []
+            res += 1
+        return 0 
+
+
