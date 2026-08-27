2b2e2d8bf8bf99c5fe96d2423d32beac7eeda56b
mrkr188 <11003007+mrkr188@users.noreply.github.com>
2026-08-27 16:30:02 -0700

    Add: swim-in-rising-water - submission-3

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+
+        N = len(grid)
+        def neibhours(node):
+            x,y = node
+            for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+                r, c = x+dx, y+dy
+                if 0 <= r < N and 0 <= c < N:
+                    yield r, c
+        
+        minHeap = [(grid[0][0], (0,0))] # (time/max-height, (r, c))
+        seen = set()
+
+        while minHeap:
+            time1, node1 = heapq.heappop(minHeap)
+
+            if node1 in seen:
+                continue
+            if node1 == (N-1, N-1):
+                return time1
+            seen.add(node1)
+
+            for node2 in neibhours(node1):
+                if node2 not in seen:
+                    time2 = grid[node2[0]][node2[1]]
+                    heapq.heappush(minHeap, (max(time1, time2), node2))
+
+
+
