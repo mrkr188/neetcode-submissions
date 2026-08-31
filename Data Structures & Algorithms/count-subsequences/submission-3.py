b6bf3b64f0fb5f94726d7ca1bd6e7e988ecca2c4
mrkr188 <11003007+mrkr188@users.noreply.github.com>
2026-08-31 14:12:07 -0700

    Add: count-subsequences - submission-3

---
+++ Data Structures & Algorithms/count-subsequences/submission-3.py
@@ -0,0 +1,73 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+        # approach: bottom-up dp matching suffixes of s and t.
+        # dp[r][c] = count of distinct subsequences of t[c:] in s[r:].
+        #
+        # evaluated grid for s = "rabb", t = "rab":
+        #
+        #                 c = 0        c = 1        c = 2        c = 3
+        #                  ('r')        ('a')        ('b')       (empty)
+        #               +------------+------------+------------+------------+
+        #  r = 0 ('r')  |     2      |     2      |     2      |     1      |
+        #               +------------+------------+------------+------------+
+        #  r = 1 ('a')  |     0      |     2      |     2      |     1      |
+        #               +------------+------------+------------+------------+
+        #  r = 2 ('b')  |     0      |     0      |     2      |     1      |
+        #               +------------+------------+------------+------------+
+        #  r = 3 ('b')  |     0      |     0      |     1      |     1      |
+        #               +------------+------------+------------+------------+
+        #  r = 4 (emp)  |     0      |     0      |     0      |     1      |
+        #               +------------+------------+------------+------------+
+
+        rows, cols = len(s), len(t)
+        dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+        # base case: an empty string t (cols) can be formed 1 way from any suffix of s
+        for r in range(rows + 1):
+            dp[r][cols] = 1
+
+        # fill table backwards starting from bottom-right
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                # choice 1: skip current character in s
+                dp[r][c] = dp[r + 1][c]
+                # choice 2: match characters, add paths that match both s[r] and t[c]
+                if s[r] == t[c]:
+                    dp[r][c] += dp[r + 1][c + 1]
+
+        # answer for full strings s[0:] and t[0:]
+        return dp[0][0]
+
+
+
+        # if len(t) > len(s):
+        #     return 0
+
+        # # i is index of s, j is index of t
+        # # (i, j) -> count of distinct subsequences
+        # dp = {} 
+
+        # # top-down dfs with memoization counts ways to match t[j:] within s[i:]
+        # # when chars match, we sum choices to match or skip s[i]; if not, we must skip s[i]
+        # def dfs(i, j):
+
+        #     # if we reach end of t, we have count 1
+        #     if j == len(t):
+        #         return 1
+        #     # when we reach end of s, count is 0 since we didn't find any match
+        #     if i == len(s):
+        #         return 0
+            
+        #     if (i, j) in dp:
+        #         return dp[(i, j)]
+            
+        #     if s[i] == t[j]:
+        #         dp[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+        #     else:
+        #         dp[(i, j)] = dfs(i + 1, j)
+            
+        #     return dp[(i, j)]
+        
+        # return dfs(0, 0)
+        
