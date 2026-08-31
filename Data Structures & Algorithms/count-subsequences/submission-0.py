52e68897db59d651041d5a42fd256e325c3aa621
mrkr188 <11003007+mrkr188@users.noreply.github.com>
2026-08-31 11:33:16 -0700

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        if len(t) > len(s):
+            return 0
+
+        # i is index of s, j is index of t
+        # (i, j) -> count of distinct subsequences
+        dp = {} 
+
+        def dfs(i, j):
+
+            # if we reach end of t, we have count 1
+            if j == len(t):
+                return 1
+            # when we reach end of s, count is 0 since we didn't find any match
+            if i == len(s):
+                return 0
+            
+            if (i, j) in dp:
+                return dp[(i, j)]
+            
+            if s[i] == t[j]:
+                dp[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+            else:
+                dp[(i, j)] = dfs(i + 1, j)
+            
+            return dp[(i, j)]
+        
+        return dfs(0, 0)
+        
