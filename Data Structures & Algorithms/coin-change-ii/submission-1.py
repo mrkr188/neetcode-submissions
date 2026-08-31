3dd9e7e1431e0ce5fe01822ef2ba54527cffb8ca
mrkr188 <11003007+mrkr188@users.noreply.github.com>
2026-08-30 18:48:43 -0700

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        # since we can repeat coins, we can go from left to right 
+        # unlike - https://neetcode.io/problems/partition-equal-subset-sum/question
+        for coin in coins:
+            for a in range(1, amount+1):
+                dp[a] += dp[a - coin] if coin <= a else 0
+        return dp[amount]
